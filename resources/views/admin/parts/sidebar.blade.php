120b566aae41d12dc161d0ff695611c3574f0706
AndrewWebPhp <andreyss100@gmail.com>
2024-12-09 10:56:27 +0200

    order sidebar menu in dashboard

--- resources/views/admin/parts/sidebar.blade.php
+++ resources/views/admin/parts/sidebar.blade.php
@@ -25,40 +25,6 @@
                         </a>
                     </li>
 
-                    <li class="treeview {{ in_array($currentRoute, [
-                            'admin.car-common-settings.edit.page',
-                            'admin.one.car.page',
-                        ]) ? 'active' : '' }}">
-                        <a href="javascript:void(0)">
-                            <i class='fa fa-cog'></i>
-                            <span>{{ trans('admin.settings') }}</span>
-                            <i class="fa fa-angle-right"></i>
-                        </a>
-                        <ul class="treeview-menu">
-                            <li @if( $currentRoute === 'admin.car-common-settings.edit.page' ) class="active"@endif><a href="{{ route('admin.car-common-settings.edit.page') }}">{{ trans('admin.car_common_settings') }}</a></li>
-                        </ul>
-                    </li>
-
-
-                    <li @if( $currentRoute === 'article.index' ) class="active"@endif>
-                        <a href="{{ route('article.index') }}">
-                            <i class='fa fa-book'></i>
-                            <span>{{ trans('admin.blog') }}</span>
-                        </a>
-                    </li>
-                    <li @if( $currentRoute === 'admin.automatch.index' ) class="active"@endif>
-                        <a href="{{ route('admin.automatch.index') }}">
-                            <i class="fa fa-heart"></i>
-                            <span>Automatch</span>
-                        </a>
-                    </li>
-                    <li @if( $currentRoute === 'client.index' ) class="active"@endif>
-                        <a href="{{ route('client.index') }}">
-                            <i class='fa fa-user'></i>
-                            <span>{{ trans('admin.clients_history') }}</span>
-                        </a>
-                    </li>
-
                     <li @if( $currentRoute === 'car.index' ) class="active"@endif>
                         <a href="{{ route('car.index') }}">
                             <i class='fa fa-car'></i>
@@ -73,12 +39,48 @@
                         </a>
                     </li>
 
+                    <li @if( $currentRoute === 'client.index' ) class="active"@endif>
+                        <a href="{{ route('client.index') }}">
+                            <i class='fa fa-user'></i>
+                            <span>{{ trans('admin.clients_history') }}</span>
+                        </a>
+                    </li>
+
+                    <li @if( $currentRoute === 'admin.automatch.index' ) class="active"@endif>
+                        <a href="{{ route('admin.automatch.index') }}">
+                            <i class="fa fa-heart"></i>
+                            <span>Automatch</span>
+                        </a>
+                    </li>
+
+                    <li @if( $currentRoute === 'article.index' ) class="active"@endif>
+                        <a href="{{ route('article.index') }}">
+                            <i class='fa fa-book'></i>
+                            <span>{{ trans('admin.blog') }}</span>
+                        </a>
+                    </li>
+                    
                     <li @if(Route::is('admin.pages.*')) class="active"@endif>
                         <a href="{{ route('admin.pages.index') }}">
                             <i class="fa fa-file-powerpoint-o"></i>
                             <span>Список FAQs</span>
                         </a>
                     </li>
+
+                    <li class="treeview {{ in_array($currentRoute, [
+                            'admin.car-common-settings.edit.page',
+                            'admin.one.car.page',
+                        ]) ? 'active' : '' }}">
+                        <a href="javascript:void(0)">
+                            <i class='fa fa-cog'></i>
+                            <span>{{ trans('admin.settings') }}</span>
+                            <i class="fa fa-angle-right"></i>
+                        </a>
+                        <ul class="treeview-menu">
+                            <li @if( $currentRoute === 'admin.car-common-settings.edit.page' ) class="active"@endif><a href="{{ route('admin.car-common-settings.edit.page') }}">{{ trans('admin.car_common_settings') }}</a></li>
+                        </ul>
+                    </li>
+
                 </ul>
             </nav>
         </div>
