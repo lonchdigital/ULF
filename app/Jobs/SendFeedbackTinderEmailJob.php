67708b6ca52e776dab2779e38cc2efd7d4d65f10
AndrewWebPhp <andreyss100@gmail.com>
2025-01-07 17:34:35 +0200

    Making a tinder (part 2)

--- app/Jobs/SendFeedbackTinderEmailJob.php
+++ app/Jobs/SendFeedbackTinderEmailJob.php
@@ -32,22 +32,21 @@ class SendFeedbackTinderEmailJob implements ShouldQueue
      */
     public function handle(): void
     {
-        $message = 'Ім\'я клієнта: ' . $this->data['name_lead'] . '<br>' .
-                    'Телефон клієнта: ' . $this->data['phone_lead'] . '<br>'
-//                    'Обрані авто: ' . $this->data['favorite_cars'] . '<br>' .
-//                    'Сторінка: ' . $this->data['page']
-        ;
+        $message = 'Ім\'я клієнта: ' . $this->data['name_drive'] . '<br>' .
+                    'Телефон клієнта: ' . $this->data['phone_drive'] . '<br>';
 
-        $notification = new SendFeedback($this->email, $message);
-
-        Notification::route('mail', $this->email)->notify($notification);
-
-        // Notification::route('mail', $this->email)->notify(new SendFeedback($this->email, $message));
+        Notification::route('mail', $this->email)->notify(new SendFeedback($this->email, $message));
 
 
         $dataForRetailLead = [
-            'name' => $this->data['name_lead'],
-            'phone' => $this->data['phone_lead'],
+            'name' => $this->data['name_drive'],
+            'phone' => $this->data['phone_drive'],
+            'current_url' => $this->data['current_url'],
+            'utm_source' => $this->data['utm_source'],
+            'utm_medium' => $this->data['utm_medium'],
+            'utm_campaign' => $this->data['utm_campaign'],
+            'utm_term' => $this->data['utm_term'],
+            'utm_content' => $this->data['utm_content'],
         ];
 
         // send data to ULF system vue API
